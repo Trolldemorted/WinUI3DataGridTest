d1ea308f6a7b5c89b746767e6d079047f1d1f73d
Benedikt Radtke <benediktradtke@gmail.com>
2023-08-12 15:08:46 +0200

    initial commit

---
+++ winuitest2/ViewModels/TestPageViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winuitest2.ViewModels;
+
+public class SomeDataTable
+{
+    public int Id { get; set; }
+
+    public SomeData[] Data { get; set; } = new SomeData[20]
+    {
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+        new SomeData(),
+    };
+}
+
+public class SomeData
+{
+    public int Id { get; set; }
+
+    public int Col1 { get; set; }
+
+    public int Col2 { get; set; }
+
+    public int Col3 { get; set; }
+
+    public int Col4 { get; set; }
+
+    public int Col5 { get; set; }
+
+    public int Col6 { get; set; }
+
+    public int Col7 { get; set; }
+}
+
+public class TestPageViewModel
+{
+    public ObservableCollection<SomeDataTable> Tables { get; set; } = new();
+
+    public TestPageViewModel()
+    {
+        this.Tables.Add(new SomeDataTable());
+        this.Tables.Add(new SomeDataTable());
+        this.Tables.Add(new SomeDataTable());
+    }
+}
